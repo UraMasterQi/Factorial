9f422f52b6527eaced890c9d1810f432f49e48fa
UraMasterQi <pogromschik@gmail.com>
2025-09-17 18:32:48 +0200

    Update and rename faktorial 2.go to faktorial0.2.go

--- faktorial0.2.go
+++ faktorial0.2.go
@@ -1,15 +1,30 @@
 //Программа факториал числа
-//Итоговая версия 0.1
+//Итоговая версия 0.2
 
+// Добавлен цвет
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+    Blue = "\033[34m"
+	Red = "\033[31m"
+	Green  = "\033[32m"
+	Reset  = "\033[0m"
+)
+
+const (
+	launs = "--------------------"
+)
 
 func main() {
+	fmt.Println(Blue + launs)
 	fmt.Println("Приведствую! Программа Факториал числа запущенна.")
 	fmt.Println("Для выхода введите ноль")
-	fmt.Println("----------")
+	fmt.Println(launs + Reset)
 	for {
 		var numberUser, summator int
 		summator = 1
@@ -17,9 +32,10 @@ func main() {
 		_, err := fmt.Scan(&numberUser)
 
 		if err != nil {
+			fmt.Println(Red + launs)
 			fmt.Println("Увы... Произшла ошибка чтения", err)
-			fmt.Println("Пожалуйста введите числовое значение!"6)
-			fmt.Println("----------")
+			fmt.Println("Пожалуйста введите числовое значение!")
+			fmt.Println(launs + Reset)
 
 			var bak string
 			fmt.Scan(&bak)
@@ -27,16 +43,18 @@ func main() {
 		}
 
 		if numberUser == 0 {
+			fmt.Println(Blue + launs)
 			fmt.Println("Выход из программы...")
+			fmt.Println(launs + Blue)
 			break
 		}
 
 		for i := 1; i <= numberUser; i++ {
 			summator *= i
 		}
-		fmt.Println("----------")
+		fmt.Println(Green + launs)
 		fmt.Println("Факториал числа", numberUser, "=", summator)
-		fmt.Println("----------")
+		fmt.Println(launs + Reset)
 
 	}
 }
