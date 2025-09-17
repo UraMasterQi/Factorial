bbe1b6f66b7f9e1f75e1dfd3d3d8e01234cf0a68
UraMasterQi <pogromschik@gmail.com>
2025-09-17 17:49:28 +0200

    Исходная версия

---
+++ factorial.go
@@ -0,0 +1,49 @@
+//Программа факториал числа
+//Итоговая версия 0.1
+
+//ЗАДАЧИ: Доделать цвет
+
+
+package main
+
+import (
+	"fmt"
+	//"github.com/fatih/color"
+)
+
+
+func main() {
+	fmt.Println()
+	fmt.Println("Приведствую! Программа Факториал числа запущенна.")
+	fmt.Println("Для выхода введите ноль")
+	fmt.Println("----------")
+	for {
+		var numberUser, summator int
+		summator = 1
+		fmt.Println("Введи число, а программа выведет его вакториал:")
+		_, err := fmt.Scan(&numberUser)
+
+		if err != nil {
+			fmt.Println("Увы... Произшла ошибка чтения", err)
+			fmt.Println("Пожалуйста введите числовое значение!")
+			fmt.Println("----------")
+
+			var bak string
+			fmt.Scan(&bak)
+			continue
+		}
+
+		if numberUser == 0 {
+			fmt.Println("Выход из программы...")
+			break
+		}
+
+		for i := 1; i <= numberUser; i++ {
+			summator *= i
+		}
+		fmt.Println("----------")
+		fmt.Println("Факториал числа", numberUser, "=", summator)
+		fmt.Println("----------")
+
+	}
+}
